e47dbe30a65efb04638c1d8e927c101a78397dcb
hthomadev <107959524+hthomadev@users.noreply.github.com>
2022-07-13 16:50:58 -0400

    Initial commit :alien:

---
+++ WMATADATACORE/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Net.Mime;
+using Newtonsoft.Json;
+using Terminal.Gui;
+using WMATADATACORE;
+
+static class Program{
+
+    
+    static void Main()
+    {
+        Application.Init();
+        
+        var label = new Label ("WMATA EInk Train Display") {
+            X = Pos.Center (),
+            Y = Pos.Percent(20),
+            Height = 3,
+        };
+        
+        var label2 = new Label ("by hthoma") {
+            X = Pos.Center (),
+            Y = Pos.Percent(40),
+            Height = 1,
+        };
+        
+        Application.Top.Add (label);
+        Application.Top.Add (label2);
+        Application.Run ();
+        Application.Shutdown();
+        MakeRequest();
+        
+
+        Console.WriteLine("Hit ENTER to exit...");
+        Console.ReadLine();
+    }
+
+    static async void MakeRequest()
+    {
+        var client = new HttpClient();
+
+        while (true)
+        {
+            // Request headers
+            client.DefaultRequestHeaders.Add("api_key", "eca071b8985a4560aaa66f86d5307efc");
+
+
+            var uri =
+                $"https://api.wmata.com/StationPrediction.svc/json/GetPrediction/{RailPrediction.DUPONT_STATION_CODE},{RailPrediction.FOGGY_BOTTOM_STATION_CODE},{RailPrediction.METRO_CENTER_STATION_CODE},{RailPrediction.FARRAGUT_WEST_STATION_CODE}?";
+
+            var response = await client.GetAsync(uri);
+
+            RailPrediction rp = JsonConvert.DeserializeObject<RailPrediction>(response.Content.ReadAsStringAsync().Result);
+
+            rp.getWalkableTrainsOnly().ForEach(t =>
+            {
+                Console.WriteLine("Train at {0} to {1} is {2} minutes away", t.LocationName, t.DestinationName, t.Min);
+            });
+
+
+            Thread.Sleep(30000);
+        }
+
+    }
+
+
+}
+
---
+++ WMATADATACORE/RailPrediction.cs
@@ -0,0 +1,58 @@
+﻿namespace WMATADATACORE;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RailPrediction
+    {
+        
+        public static string DUPONT_STATION_CODE = "A03";
+        public static string FARRAGUT_WEST_STATION_CODE = "C03";
+        public static string FOGGY_BOTTOM_STATION_CODE = "C04";
+        public static string METRO_CENTER_STATION_CODE = "A01";
+        public static List<String> WALKABLE_STATIONS = new List<string>{ DUPONT_STATION_CODE, FARRAGUT_WEST_STATION_CODE, FOGGY_BOTTOM_STATION_CODE };
+        public static List<String> GREENSBORO_OR_BEYOND = new List<string> { "N03", "N04", "NO5", "N06" };
+        public static List<String> METROCENTER_AND_BEYOND = new List<string> { "B11","B10","B09", "B08", "B07", "B06", "B05", "B04", "B03", "B02", "B01", "A01" };
+        
+        
+        public List<Train> Trains { get; set; }
+
+        public List<Train> getTrainsToWorkOnly()
+        {
+            return Trains.FindAll(t =>
+                GREENSBORO_OR_BEYOND.Contains(t.DestinationCode) || METROCENTER_AND_BEYOND.Contains(t.DestinationCode));
+        }
+
+        public List<Train> getWalkableTrainsOnly()
+        {
+          return this.Trains.Where(t=> 
+                (t.LocationCode == DUPONT_STATION_CODE)
+                || (t.LocationCode == FARRAGUT_WEST_STATION_CODE) 
+                || (t.LocationCode == FOGGY_BOTTOM_STATION_CODE)).ToList();
+        }
+        public List<Train> getWalkableTrainsInTimeOnly()
+        {
+            return this.Trains.Where(t=> 
+                (t.LocationCode == DUPONT_STATION_CODE && int.Parse(t.Min) > 4)
+                || (t.LocationCode == FARRAGUT_WEST_STATION_CODE && int.Parse(t.Min) > 14) 
+                || (t.LocationCode == FOGGY_BOTTOM_STATION_CODE && int.Parse(t.Min) > 14)).ToList();
+        }
+        
+        
+        
+    }
+
+    public class Train
+    {
+        public string Car { get; set; }
+        public string Destination { get; set; }
+        public string DestinationCode { get; set; }
+        public string DestinationName { get; set; }
+        public string Group { get; set; }
+        public string Line { get; set; }
+        public string LocationCode { get; set; }
+        public string LocationName { get; set; }
+        public string Min { get; set; }
+    }
